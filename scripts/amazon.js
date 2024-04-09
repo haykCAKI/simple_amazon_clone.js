f25308182c279edf092456a69169d012329479d7
Herick Kumata <herickyk@gmail.com>
2024-04-09 08:30:58 -0300

    Move addCart into cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, addCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 
 
@@ -58,6 +58,17 @@ products.forEach((products) => {
    `;
 });
 
+function updateCartQuantity() {
+   let cartQuantity = 0;
+
+   cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
+   });
+
+   document.querySelector('.js-quantity-show-kekw')
+      .innerHTML = cartQuantity;
+
+}
 
 document.querySelector('.js-products-grid')
    .innerHTML = productsHTML;
@@ -68,32 +79,8 @@ document.querySelectorAll('.js-add-to-cart')
       button.addEventListener('click', () => {
          const productId = button.dataset.productId;
 
-         let matchingItem;
-
-         cart.forEach((item) => {
-            if (productId === item.productId) {
-               matchingItem = item;
-            }
-         });
-
-         if (matchingItem) {
-            matchingItem.quantity += 1;
-         } else {
-            cart.push({
-               productId: productId,
-               quantity: 1,
-            });
-         }
-
-         let cartQuantity = 0;
-
-         cart.forEach((item) => {
-            cartQuantity += item.quantity;
-         });
-
-         document.querySelector('.js-quantity-show-kekw')
-            .innerHTML = cartQuantity;
+         addCart(productId);
+         updateCartQuantity();
 
       });
    });
-
