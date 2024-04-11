a129717d290516b8767c68b3151635914d2a9ce6
Herick Kumata <herickyk@gmail.com>
2024-04-11 08:00:56 -0300

    fix the priceCents bug

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,5 +1,6 @@
 export function formatCurrency(priceCents) {
-  return (Math.round(priceCents / 100)).toFixed(2);
+  const dollars = (Math.round(priceCents / 100)).toFixed(2);
+  return `$${dollars}`;
 }
 
 export default formatCurrency;
